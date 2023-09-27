83e5f460c900061091f778e04304a6c6d63b8e06
nizanhulq <nizandhiaulhaq@mail.ugm.ac.id>
2023-09-27 10:21:27 +0700

    Add praktikum react form

---
+++ 17_React Form/Praktikum/react-fundamental/src/pages/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { useAlert, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
+import CreateProduct from './product/CreateProduct'
+
+const options = {
+  position: 'top right',
+  timeout: 2000,
+  offset: '30px',
+};
+
+function App() {
+
+  return (
+    <AlertProvider template={AlertTemplate} {...options}>
+      <CreateProduct />
+    </AlertProvider>
+  )
+}
+
+export default App
